1afd3ef670108383afa3abc712c3660de3b32946
Andy Li <boruil@cs.washington.edu>
2016-01-15 03:32:32 -0800

    update

--- HelloMedicalResearch/SurveyTaskController.swift
+++ HelloMedicalResearch/SurveyTaskController.swift
@@ -30,6 +30,13 @@ class SurveyTaskController: UIViewController {
         taskViewController.delegate = self
         presentViewController(taskViewController, animated: true, completion: nil)
     }
+    
+    @IBAction func microphoneTapped(sender : AnyObject) {
+        let taskViewController = ORKTaskViewController(task: MicrophoneTask, taskRunUUID: nil)
+        taskViewController.delegate = self
+        taskViewController.outputDirectory = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String, isDirectory: true)
+        presentViewController(taskViewController, animated: true, completion: nil)
+    }
 }
 
 // let view controller implements ORKTaskViewControllerDelegate
